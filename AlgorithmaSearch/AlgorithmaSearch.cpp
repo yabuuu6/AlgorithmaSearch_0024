2a0dc4d604109c722c0f4a4653a3bf5c4f4e106e
buyaramadhan <ytchochote@gmail.com>
2024-04-05 14:57:45 +0700

    commit 3

--- AlgorithmaSearch/AlgorithmaSearch.cpp
+++ AlgorithmaSearch/AlgorithmaSearch.cpp
@@ -17,3 +17,10 @@ void input()
 
 			cout << "\nArray should have minimum 1 and maximum 20 elements.\n\n";
 	}
+	// Accept array elements
+
+	cout << "\n--------------------\n";
+	cout << "Enter array element\n";
+	cout << "\n---------------------\n";
+	for (i = 0; i < n; i++)
+
