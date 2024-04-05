005168b332763d34fea708aff8aff7abac9efe88
buyaramadhan <ytchochote@gmail.com>
2024-04-05 14:57:16 +0700

    commit 2

--- AlgorithmaSearch/AlgorithmaSearch.cpp
+++ AlgorithmaSearch/AlgorithmaSearch.cpp
@@ -5,4 +5,15 @@ int arr[20]; // Array to be searched
 int n; // Number of elements in the array
 int i; // Index of array element
 
+void input()
+{
+	while (true)
+	{
+		cout << "Enter the number of elements in the array: ";
+		cin >> n;
+		if ((n > 0) && (n <= 20))
+			break;
+		else
 
+			cout << "\nArray should have minimum 1 and maximum 20 elements.\n\n";
+	}
