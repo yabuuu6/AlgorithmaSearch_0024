2073856cf10ebe795e410f7ead7e9f16e30eed28
buyaramadhan <ytchochote@gmail.com>
2024-04-05 14:56:20 +0700

    Add project files.

---
+++ AlgorithmaSearch/AlgorithmaSearch.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int arr[20]; // Array to be searched
+int n; // Number of elements in the array
+int i; // Index of array element
+
+
---
+++ enc_temp_folder/612c623443f73d4659e0b589896bc60/AlgorithmaSearch.cpp
@@ -0,0 +1,82 @@
+#include <iostream>
+using namespace std;
+
+int arr[20]; // Array to be searched
+int n; // Number of elements in the array
+int i; // Index of array element
+
+void input()
+{
+	while (true)
+	{
+		cout << "Enter the number of elements in the array: ";
+		cin >> n;
+		if ((n > 0) && (n <= 20))
+			break;
+		else
+
+			cout << "\nArray should have minimum 1 and maximum 20 elements. \n";
+	}
+	// Accept array elements
+
+	cout << "\n--------------------\n";
+	cout << "Enter array element\n";
+	cout << "\n---------------------\n";
+	for (i = 0; i < n; i++)
+	{
+		cout << "<" << (i + 1) << "> ";
+		cin >> arr[i];
+	}
+}
+void LinearSearch()
+{
+	char ch;
+	int ctr; // Number of comparisons
+	int item;
+
+	do
+
+	{
+		// Accept the number to be searched
+		cout << "\nEnter the element you want to search: "; // step 1
+		cin >> item;
+
+		ctr = 0;
+		i = 0; // step 2
+		while (i < n) // step 3
+		{
+			ctr++;
+			if (arr[i] == item)
+			{
+				cout << "\n" << item << " found at position " << (i + 1) << endl; // step 6
+				break;
+
+			}
+			i++; // step 4
+		}
+		if (i == n) // step 5
+			cout << "\n" << item << " not found in the array\n";
+		cout << "\nNumber of comparisons: " << ctr << endl;
+
+		cout << "\nContinue search (y/n):";
+		cin >> ch;
+
+	} while ((ch == 'y') || (ch == 'Y'));
+}
+
+
+void display()
+
+{
+
+	cout << "Array elements : ";
+	for (i = 0; i < n; i++)
+		cout << arr[i] << "";
+	cout << endl;
+}
+int main()
+{
+	input();
+	LinearSearch();
+		display();
+}
